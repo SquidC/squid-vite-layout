ec5876c1ce33b57869be5ecf4f0eef28127d53c0
grandpawang <907415276@qq.com>
2021-01-10 21:06:57 +0800

    feat: 添加types

--- playground/src/test.layout.tsx
+++ playground/src/test.layout.tsx
@@ -1,18 +1,25 @@
 import { LayoutConfig } from "../../plugin/types"
 
-const pageConfig: LayoutConfig = {
+const pageConfig: LayoutConfig<any> = {
   layout: "TestLayout",
+  title: "hello",
+  tools: ["add", "refresh"], 
+  rawTools: ["edit", "view", "delete"], 
+  rawToolsPostion: "body", 
+  pageLayout: ["prev", "jumper", "total", "next"],
   // crud api
-  add: "https://localhost/add",
-  del: "https://localhost/del",
-  alt: "https://localhost/alt",
-  sel: "https://localhost/sel",
+  api: {
+    add: "https://localhost/add",
+    del: "https://localhost/del",
+    alt: "https://localhost/alt",
+    sel: "https://localhost/sel",
+  },
   // 处理按钮事件
-  handleClick: (mode, raw, ids) => {
+  handleAction: (mode, raw, ids) => {
 
   },
   // 页面配置
-  config: [
+  colunms: [
     {
       label: "hello",
       mode: ["add", "table"],
--- plugin/types.ts
+++ plugin/types.ts
@@ -1,43 +1,108 @@
+// layout 提供的所有用户接口事件
 type ActionType = 
     | "add"
-    | "del"
-    | "alt"
-    | "sel"
-    | "flush"
-    | "submit"
-    | "cancel"
+    | "edit"
+    | "view"
+    | "delete"
+    | "more"
+    | "activate"
+    | "forbidden"
+    | "refresh"
+    | "export"
+    | "search"
+    | "compose"
+    | "prev"
+    | "next"
+    | "pageSize"
+    | "page"
+
+// 工具栏
+type Tools = 
+    | "refresh"
+    | "add"
+
+// 行工具栏
+type RawTools = 
+    | "edit"
+    | "view"
+    | "delete"
+
+// 分页
+type Pagination = 
+    |"prev"
+    | "pager"
+    | "next"
+    | "jumper"
+    | "->"
+    | "total"
+
+// 行工具栏位置
+type RawToolsPostion = 
+    | "header"
+    | "body"
+
+
+/**
+ * 服务器api
+*/
+interface ServerAPI {
+    /**
+     * 添加数据url
+    */
+   add: string;
+   /**
+    * 删除数据url
+   */
+   del: string;
+   /**
+    * 修改数据url
+   */
+   alt: string;
+   /**
+    * 查询数据url
+   */
+   sel: string;
+}
 
 /**
  * 用于调用layout组件
  * <layoutConfig.layout :config="layoutConfig" />
 */
-export interface LayoutConfig {
+export interface LayoutConfig<T> {
     /**
      * layout组件名称
     */
     layout: string;
     /**
-     * 添加数据url
+     * 标题
     */
-    add: string;
+    title: string;
     /**
-     * 删除数据url
+     * 页面字段配置
     */
-    del: string;
+    colunms: Array<T>;
     /**
-     * 修改数据url
+     * 页面工具栏配置
     */
-    alt: string;
+    tools: Array<Tools>;
     /**
-     * 查询数据url
+     * 请求api的url
     */
-    sel: string;
+    api: ServerAPI;
+    /**
+     * 行工具栏配置
+    */
+    rawTools: Array<RawTools>;
+    /**
+     * 工具类位置
+    */
+    rawToolsPostion: RawToolsPostion;
+    /**
+     * 分页器
+    */
+    pageLayout: Array<Pagination>;
     /**
      * layout所有事件hooks
     */
-    handleClick: (mode: ActionType, raw: unknown, ids: Array<number|string>) => void;
-    /**
-     * 页面所有配置
-    */
-    config: Array<any>;
+    handleAction: (mode: ActionType, raw: unknown, ids?: Array<number|string>) => void;
 }
